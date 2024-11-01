7e59de729c6ab539c412a977ba892d286c15a43e
dm <dmishra@gmail.com>
2024-11-01 11:00:28 +0530

    seminar

--- src/data/seminarspage.ts
+++ src/data/seminarspage.ts
@@ -40,14 +40,7 @@ export const SeminarPageData:seminarPageSpec = {
         
         
 
-        {
-            title: `TBA`,
-            speaker: `Swagata Bhattacharjee (Jindal Global University)`,
-            datetime: `11:30 AM, 15 November, 2024`,
-            venue: ` Seminar Room 2 `,
-            description:``
-        },
-
+        
         {
             title: `Collateral, Property Rights and Disparities in Credit Access`,
             speaker: `Nirupama Kulkarni (CAFRAL)`,
@@ -61,7 +54,7 @@ export const SeminarPageData:seminarPageSpec = {
             speaker: `Bipasha Maity, Ashoka University`,
             datetime: `11:30 AM, 29 November, 2024`,
             venue: ` Seminar Room 2 `,
-            description:``
+            description:`Using the collective household model framework and a recent structural estimation methodology, we estimate how intra-household resource shares of individual members in India vary by exogenously varying soil texture. In general we find that relative to men, women and children have substantially lower access to consumption resources within Indian households. However, gender gap in resource shares and consequently relative female poverty is found to be substantially lower in households in clayey relative to loamy soil regions. Despite widespread prevalence of child poverty across soil textures, children’s resource shares are also found to be modestly higher in clayey soil regions. As clayey soil has been associated with higher relative female employment and overall gender norms favourable to women, our findings suggest that soil texture also plays an important role in facilitating improvement of relative female bargaining power within households.`
         },
 
         {
@@ -71,6 +64,24 @@ export const SeminarPageData:seminarPageSpec = {
             venue: ` Seminar Room 2 `,
             description:``
         },
+
+        {
+            title: `Matching in co-operative education programs: An experimental study`,
+            speaker: `Bradley Ruffle, McMaster University`,
+            datetime: `11:30 AM, 13 December, 2024`,
+            venue: ` Seminar Room 2 `,
+            description:`Most Canadian universities and colleges participate in co-operative education programs
+whereby each year more than 80,000 co-op students alternate between dedicated for-credit work
+terms and school terms. Many of these programs use a minimum sums algorithm to match students
+to jobs. We show that this algorithm and all its variations may produce unstable outcomes. We
+compare experimentally the properties of this algorithm and seemingly improved variations with
+the deferred acceptance algorithm. While the improved versions of the minimum sums algorithm
+sometimes lead to more truthful reporting of preferences and increase the likelihood of a stable
+assignment, they all fare worse than the deferred acceptance algorithm. Our data reveal that the
+superior outcomes associated with deferred acceptance are the result of both the algorithm itself
+and the behavioral responses it elicits.`
+        },
+
         {
             title: `TBA`,
             speaker: `Nishant Ravi, Indian School of Business`,
@@ -78,6 +89,15 @@ export const SeminarPageData:seminarPageSpec = {
             venue: ` Seminar Room 2 `,
             description:``
         },
+
+        {
+            title: `TBA`,
+            speaker: `Swagata Bhattacharjee (Jindal Global University)`,
+            datetime: `11:30 AM, 31 January, 2025`,
+            venue: ` Seminar Room 2 `,
+            description:``
+        },
+
 
         {
             title: `Climate change and human capital: Evidence from the Indian Census`,
